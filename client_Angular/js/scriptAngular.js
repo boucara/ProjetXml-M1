ec1e6ccb87fc0179eb388520b01ec8d1fc862c52
Jux <julien.hubert.info@gmail.com>
2016-12-11 17:44:39 +0100

    Obtenir département par région cliquée via webService AngularJS-Node

--- client_Angular/js/scriptAngular.js
+++ client_Angular/js/scriptAngular.js
@@ -1,6 +1,5 @@
 var myApp = angular.module('myApp',[]);
-  myApp.controller('MainController', ['$scope', '$http', function($scope, $http) { 
-  console.log("test");
+  myApp.controller('MainController', ['$scope', '$http', function($scope, $http) {
     $scope.GETobtenirRegions = function() {
       $http({
         method : 'GET',
@@ -14,7 +13,26 @@ var myApp = angular.module('myApp',[]);
           $scope.regions = response.data; // On stock le JSON obtenu par le WebService
           },
           function errorCallback(response) {
-            alert("Echec de l'import des données");
+            alert("Echec de l'import des régions");
         })
      };
+
+     $scope.GETobtenirDepartementsInRegion = function(region) {
+      region = region.substring(0,1).toUpperCase()+region.substring(1);
+       $http({
+         method : 'GET',
+         url : 'http://localhost:1337/obtenirDepartement',
+         params: {region: region},
+
+         headers: {
+           'Content-Type': 'application/JSON'
+         },
+       }).then(
+           function successCallback(response) {
+           $scope.departementsInRegion = response.data; // On stock le JSON obtenu par le WebService
+           },
+           function errorCallback(response) {
+             alert("Echec de l'import des départements");
+         })
+      };
   }]);
