77131dce93823ed0861ed87405317b2d0469aae9
Jux <julien.hubert.info@gmail.com>
2016-12-13 23:47:45 +0100

    Affichage de la photo du monument dans un onglet et sur la GoogleMap

--- client_Angular/js/scriptAngular.js
+++ client_Angular/js/scriptAngular.js
@@ -1,6 +1,7 @@
 var myApp = angular.module('myApp',[]);
 
   myApp.controller('MainController', ['$scope', '$http',  function($scope, $http) {
+
     $scope.GETobtenirRegions = function() {
       $http({
         method : 'GET',
@@ -31,10 +32,6 @@ var myApp = angular.module('myApp',[]);
        }).then(
            function successCallback(response) {
            $scope.departementsInRegion = response.data; // On stock le JSON obtenu par le WebService
-           $scope.lat = 43.5576;
-           $scope.lng = 5.3595;
-           $scope.img = "http://commons.wikimedia.org/wiki/Special:FilePath/%C3%89guilles%20-%20Colline%20de%20Pierredon.jpg?width=200";
-
            },
            function errorCallback(response) {
              alert("Echec de l'import des départements");
@@ -42,20 +39,20 @@ var myApp = angular.module('myApp',[]);
       };
 
 
-      $scope.GETobtenirCoordonneesGPSDuMonument = function(departement) {
-       //region = region.substring(0,1).toUpperCase()+region.substring(1);
+      $scope.GETobtenirCoordonneesGPSDuMonument = function(monumentID) {
         $http({
           method : 'GET',
-          url : 'http://localhost:1337/obtenirCoordonneesGPSMonumentsDansDepartement',
-          params: {departement: departement},
+          url : 'http://localhost:1337/obtenirCoordonneesGPSMonument',
+          params: {monumentID: monumentID},
 
           headers: {
             'Content-Type': 'application/JSON'
           },
         }).then(
             function successCallback(response) {
-            $scope.coordonneesGPS = response.data; // On stock le JSON obtenu par le WebService
-            placerMarkers($scope.coordonneesGPS);
+            $scope.coordonneesGPS = response.data[0]; // On stock le JSON obtenu par le WebService
+            $scope.image = response.data[1]; // On stock le JSON obtenu par le WebService
+            placerMarkers($scope.coordonneesGPS,$scope.image,$scope.nomMonument);
             },
             function errorCallback(response) {
               alert("Echec de l'import des départements");
@@ -64,7 +61,6 @@ var myApp = angular.module('myApp',[]);
 
 
        $scope.GETobtenirMonumentsDansDepartement = function(departement) {
-        //region = region.substring(0,1).toUpperCase()+region.substring(1);
          $http({
            method : 'GET',
            url : 'http://localhost:1337/obtenirMonumentsDansDepartement',
@@ -74,7 +70,24 @@ var myApp = angular.module('myApp',[]);
            },
          }).then(
              function successCallback(response) {
-             $scope.monumentsInDepartement = response.data; // On stock le JSON obtenu par le WebService
+             $scope.reponse = response.data;
+             $scope.monuments = [];
+             $scope.nomMonument;
+             for(var i=0 ; i<$scope.reponse.length ; i++)
+             {
+               var start = $scope.reponse[i].indexOf("<TICO>")+6;
+               var end = $scope.reponse[i].indexOf("</TICO>");
+               $scope.nomMonument = $scope.reponse[i].slice(start,end);
+
+               var start = $scope.reponse[i].indexOf("<REF>")+5;
+               var end = $scope.reponse[i].indexOf("</REF>");
+               var refMonu = $scope.reponse[i].slice(start,end);
+
+               $scope.monuments.push([$scope.nomMonument,refMonu]);
+             }
+
+             $scope.monumentsInDepartement = $scope.monuments.sort();
+
              },
              function errorCallback(response) {
                alert("Echec de l'import des monuments");
--- serveur_web_nodejs/srv.js
+++ serveur_web_nodejs/srv.js
@@ -9,26 +9,17 @@ var bodyParser = require('body-parser');
 var fs = require("fs");
 var xml = require('xml');
 var Connection = require('./existdb-node-master/index.js');
-// test inclusion sparql
-
-
-
-
+var format = require('string-format');
 var SparqlClient = require('sparql-client');
 var util = require('util');
 var endpoint = 'https://query.wikidata.org';
-
 var wdk = require('wikidata-sdk');
 var http = require('http');
-
 http.post = require('http-post');
 var request = require('request');
 var parser = require('json-parser');
 var wait=require('wait.for');
 var async = require('async');
-var yolo = "zzzzz";
-var IDMonument;
-var tab2 = new Array();
 
 var options = {
     host: "localhost",
@@ -36,14 +27,16 @@ var options = {
     rest: "/exist/rest",
     auth: "admin:"
 };
+
 var connection = new Connection(options);
 const PORT=1337;
 app.listen(PORT, function () {
   console.log('Example app listening on port '+PORT);
 });
+
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(express.static('../client_angular'));
 
@@ -54,23 +47,24 @@ app.get('/', function (req, res, err) {
 app.get('/obtenirRegions', function (req, res) {
   res.set('Content-Type', 'text/json');
   var xquery = fs.readFileSync("foncRegions.xql", "UTF-8");
-  var getRegions = connection.query(xquery, { chunkSize: 20 });  // chunckSize = nombre de doc à retourner
+  var getRegions = connection.query(xquery, { chunkSize: 20 });
   getRegions.on("error", function(err) {
       console.log("An error occurred: " + err);
   });
   var donneesBrutes = new Array();
   getRegions.bind().each(function(item, hits, offset) {
-      donneesBrutes.push(item.toLowerCase());
+      donneesBrutes.push(item);
       if(offset == hits)
       {
-        res.send(ObtainUniqueValues(donneesBrutes));
+        res.send(donneesBrutes.sort());
       }
   });
 });
 
 app.get('/obtenirDepartement', function (req, res) {
+  var getDeparte = connection.query('distinct-values(for $i in collection("monuments")//row where $i//REG="'+req.query.region+'" order by $i//DPT return $i//DPT)', { chunkSize: 20 });
   console.log(req.query.region);
-  var getDeparte = connection.query("distinct-values(for $i in collection('monuments')//row where $i//REG='"+req.query.region+"' order by $i//DPT return $i//DPT)", { chunkSize: 20 });  // chunckSize = nombre de doc à retourner
+  console.log(format(req.query.region));
   getDeparte.on("error", function(err) {
       console.log("An error occurred: " + err);
   });
@@ -79,7 +73,7 @@ app.get('/obtenirDepartement', function (req, res) {
       donneesBrutesDeparte.push(item);
       if(offset == hits)
       {
-        res.send(ObtainUniqueValues(donneesBrutesDeparte));
+        res.send(donneesBrutesDeparte.sort());
       }
   });
 });
@@ -87,48 +81,26 @@ app.get('/obtenirDepartement', function (req, res) {
 
 
 app.get('/obtenirMonumentsDansDepartement', function (req, res) {
-  var getMonumentDansDepartement = connection.query("for $i in collection('monuments')//row where $i//DPT='"+req.query.departement+"' order by $i//DPT return $i",  { chunkSize: 20 });
+  var getMonumentDansDepartement = connection.query('for $i in collection("monuments")//row where $i//DPT="'+req.query.departement+'" order by $i//DPT return $i',  { chunkSize: 20 });
   getMonumentDansDepartement.on("error", function(err) {
       console.log("An error occurred: " + err);
   });
   var donneesBrutesMonu = new Array();
-  //console.log(getMonumentDansDepartement.length);
   getMonumentDansDepartement.bind().each(function(item, hits, offset) {
       donneesBrutesMonu.push(item);
       if(offset == hits)
       {
-        //console.log(donneesBrutesMonu);
         res.set('Content-Type', 'text/json');
-        res.send(JSON.stringify(donneesBrutesMonu));
+        res.send(donneesBrutesMonu);
       }
   });
 
 
 });
 
-app.get('/obtenirCoordonneesGPSMonumentsDansDepartement', function (req, res) {
-  var getNumeroMonumentDansDepartement = connection.query("distinct-values(for $i in collection('monuments')//row where $i//DPT='"+req.query.departement+"' order by $i//DPT return $i//REF)",  { chunkSize: 20 });
-  getNumeroMonumentDansDepartement.on("error", function(err) {
-      console.log("An error occurred: " + err);
-  });
-  var donneesBrutesRef = new Array();
-  getNumeroMonumentDansDepartement.bind().each(function(item, hits, offset) {
-      donneesBrutesRef.push(item);
-      if(offset == hits)
-      {
-        var coordonneesGPS = null;
-        console.log("nb réponse "+donneesBrutesRef.length);
-        var positions = new Array();
-          //console.log(donneesBrutesRef[i]);
-          //function getGPS(donneesBrutesRef)
-          //{
-            var i=0;
-            //var compteur=0;
-            while(i<10)
-            {
-              var sparql ="SELECT ?label ?coord ?image WHERE { ?subj wdt:P380  '"+donneesBrutesRef[i]+"' . ?subj wdt:P18 ?image .?subj wdt:P625 ?coord .?subj rdfs:label ?label SERVICE wikibase:label { bd:serviceParam wikibase:language 'en'. }}";
+app.get('/obtenirCoordonneesGPSMonument', function (req, res) {
+              var sparql ='SELECT ?label ?coord ?image WHERE { ?subj wdt:P380  "'+req.query.monumentID+'" . ?subj wdt:P18 ?image .?subj wdt:P625 ?coord .?subj rdfs:label ?label SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }}';
               var url = wdk.sparqlQuery(sparql);
-              IDMonument = donneesBrutesRef[i];
               request('http://query.wikidata.org/sparql?format=json&query='+sparql, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                   var object = parser.parse(body);
@@ -138,88 +110,14 @@ app.get('/obtenirCoordonneesGPSMonumentsDansDepartement', function (req, res) {
                     coordonneesGPS = coordonneesGPS.split("(");
                     coordonneesGPS = coordonneesGPS[1].split(")");
                     coordonneesGPS = coordonneesGPS[0].split(" ");
-                    //positions.push(coordonneesGPS);
-                    //console.log(coordonneesGPS[0]+" "+coordonneesGPS[1]);
-                    //compteur++;
-                    //res.send(coordonneesGPS);
-                    //console.log(coordonneesGPS);
-                    tab2.push(coordonneesGPS);
+                    var image = object.results.bindings[0].image.value;
+                    res.send([coordonneesGPS,image]);
                   }
                   else
                   {
-                      //console.log("Pas de monument pour cet ID");
-                      //compteur++;
+                      console.log("Pas de monument pour cet ID");
+                      res.send("Pas de monument pour cet ID");
                   }
-                  //tab2.push(coordonneesGPS);
-                  //console.log(coordonneesGPS);
-                  //console.log(tab2);
-                }
-              });
-              i++;
-            }
-            while(true)
-            {
-              //console.log(i);
-              if(i==10)
-              {
-                res.send(tab2);
               }
-            }
-            //setTimeout(console.log("FINIIIIIIIIIII"), 300000);
-          }
-  });
+            });
 });
-
-
-/*
-app.get('/obtenirVille', function (req, res) {
-  console.log(req.query.region);
-  var getDeparte = connection.query("distinct-values(for $i in collection('monuments')//row where $i//REG='"+req.query.region+"' order by $i//DPT return $i//DPT)", { chunkSize: 20 });  // chunckSize = nombre de doc à retourner
-  getDeparte.on("error", function(err) {
-      console.log("An error occurred: " + err);
-  });
-  var donneesBrutesDeparte = new Array();
-  getDeparte.bind().each(function(item, hits, offset) {
-      donneesBrutesDeparte.push(item);
-      if(offset == hits)
-      {
-        res.send(ObtainUniqueValues(donneesBrutesDeparte));
-      }
-  });
-});*/
-
-
-
-function getRegions()
-{
-  var xquery = fs.readFileSync("fonc.xql", "UTF-8");
-  var getRegions = connection.query(xquery, { chunkSize: 20 });  // chunckSize = nombre de doc à retourner
-  getRegions.on("error", function(err) {
-      console.log("An error occurred: " + err);
-  });
-  var donneesBrutes = new Array();
-  getRegions.bind().each(function(item, hits, offset) {
-      donneesBrutes.push(item.toLowerCase());
-      if(offset == hits)
-      {
-        GestionTailleReponse(ObtainUniqueValues(donneesBrutes).length);
-        return ObtainUniqueValues(donneesBrutes);
-      }
-  });
-}
-
-function GestionTailleReponse(hits)
-{
-  console.log("Nombre de réponse : "+hits);
-}
-
-function ObtainUniqueValues(tab)
-{
-    return tab.filter((v, i, a) => a.indexOf(v) === i).sort();
-}
-/*query.bind().each(function(item, hits, offset) {
-    //console.log("Item %d out of %d:", offset, hits);
-    console.log(item);
-});*/
-
-// Obtenir une liste de livres, y compris leurs genres. La liste sera formaté ou groupés par genres
